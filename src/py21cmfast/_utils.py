0123a8500cf2cbabeb6d5abc63b8583959f273f7
junsong-cang <cangjunsong@outlook.com>
2023-11-07 10:59:08 +0800

    silenced warnings

--- src/py21cmfast/_utils.py
+++ src/py21cmfast/_utils.py
@@ -397,11 +397,15 @@ class StructWithDefaults(StructWrapper):
                 setattr(self, "_" + k, v)
 
         if kwargs:
+            pass
+            '''
+            JSC: silencing warnings
             warnings.warn(
                 "The following parameters to {thisclass} are not supported: {lst}".format(
                     thisclass=self.__class__.__name__, lst=list(kwargs.keys())
                 )
             )
+            '''
 
     def convert(self, key, val):
         """Make any conversions of values before saving to the instance."""
