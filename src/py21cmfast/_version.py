9bfb0f5cfb258775b8816c7943bca672f09e8048
junsong-cang <cangjunsong@outlook.com>
2025-09-23 10:46:38 +0200

    changed version?

--- src/py21cmfast/_version.py
+++ src/py21cmfast/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '3.2.1.dev83+g37fc2fc0f.d20250817'
-__version_tuple__ = version_tuple = (3, 2, 1, 'dev83', 'g37fc2fc0f.d20250817')
+__version__ = version = '3.2.1.dev84+gcbab9752a.d20250923'
+__version_tuple__ = version_tuple = (3, 2, 1, 'dev84', 'gcbab9752a.d20250923')
 
-__commit_id__ = commit_id = 'g37fc2fc0f'
+__commit_id__ = commit_id = 'gcbab9752a'
